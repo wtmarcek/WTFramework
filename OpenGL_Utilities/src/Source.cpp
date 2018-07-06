4a37e46a032038435a653f03c9a45f12ac1dfeaf
Marcin <wtmarcek@gmail.com>
2018-07-06 20:40:41 +0200

    basic shader, shader utilities

--- OpenGL_Utilities/src/Source.cpp
+++ OpenGL_Utilities/src/Source.cpp
@@ -1,7 +1,6 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
-
+#include "ShaderUtilities.h"
 
 
 int main(void)
@@ -14,7 +13,7 @@ int main(void)
 
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Kupa World", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "Kupa", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -27,6 +26,25 @@ int main(void)
 	{
 		std::cout << "#ERROR: glewInit() != GLEW_OK" << std::endl;
 	}
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	float tris[6] =
+	{
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	};
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6, tris, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray (0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+	ShaderProgramSource source = ParseShader("src/Shaders/Basic.shader");
+	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+	glUseProgram(shader);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -34,11 +52,7 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
@@ -47,6 +61,7 @@ int main(void)
 		glfwPollEvents();
 	}
 
+	glDeleteProgram(shader);
 	glfwTerminate();
 	return 0;
 }
